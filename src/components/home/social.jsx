e58ffc935e78a53bd96ced6e5f4f7b9e39aa1c3d
Lhasang Tulku Lama <133319598+Mrlhasang18@users.noreply.github.com>
2024-04-27 20:09:55 +0545

    Add files via upload

---
+++ src/components/home/social.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const social = () => {
+  return (
+    <div className="home__social">
+      <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+        <i className="uil uil-instagram"></i>
+      </a>
+      <a href="https://www.linkedin.com/in/tulku18/" className="home__social-icon" target='_blank'>
+      <i className="uil uil-linkedin"></i>
+      </a>
+      <a href="https://github.com/Mrlhasang18" className="home__social-icon" target='_blank'>
+      <i className="uil uil-github-alt"></i>
+      </a>
+    </div>
+  );
+};
+
+export default social;
