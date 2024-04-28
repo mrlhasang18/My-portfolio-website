0466f75e9f067bad0cbfb3384ed0c2c05e69a87d
Lhasang Tulku Lama <133319598+Mrlhasang18@users.noreply.github.com>
2024-04-28 20:44:19 +0545

    Update social.jsx

--- src/components/home/social.jsx
+++ src/components/home/social.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const social = () => {
   return (
     <div className="home__social">
-      <a href="https://www.instagram.com/" className="home__social-icon" target="_blank">
+      <a href="https://www.instagram.com/lhasang.t.lama/" className="home__social-icon" target="_blank">
         <i className="uil uil-instagram"></i>
       </a>
       <a href="https://www.linkedin.com/in/tulku18/" className="home__social-icon" target='_blank'>
