884bfca67e146e1ff7708be4bf32a48f9dddae72
Lhasang Tulku Lama <lamalhasang256@gmail.com>
2024-08-07 18:27:59 +0545

    Updated

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import "./App.css";
+import { Analytics } from "@vercel/analytics/react"
 import "./components/header/darkMode.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
@@ -26,7 +27,7 @@ const App = () => {
         <Services/>
         <Qualification/>
         <Work />
-       
+       <Analytics/>
         <Contact />
       </main>
       <Footer />
